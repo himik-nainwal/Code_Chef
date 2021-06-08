c0b5fdde945d7bba93bebe9795f6196be55fbe2d
Himith <59734430+Himith@users.noreply.github.com>
2021-06-08 20:52:27 +0530

    Updated Cheating Time

--- May CodeChef Starters 2021 Division 3 (Rated)/CTIME.cpp
+++ May CodeChef Starters 2021 Division 3 (Rated)/CTIME.cpp
@@ -45,3 +45,47 @@ Example case 3: Chef can google for 2 minutes (in the time interval [10,12]) and
 
 */
 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ff first
+#define ss second
+                             
+int main(void)
+{
+       ll test_cases;
+       cin>>test_cases;
+       while(test_cases--){
+              ll n,m,k,f,i;
+              cin>>n>>k>>f;
+              vector<pair<ll,ll>> pq;
+              for(i=0;i<n;i++){
+                        ll a,b;
+                        cin>>a>>b;
+                        pq.push_back({a,b});
+              }
+              sort(pq.begin(),pq.end());
+              ll ans=0;
+              ans+=pq[0].ff;
+              ll x=pq[0].ff,y=pq[0].ss;
+              for(i=1;i<n;i++)
+              {
+                        if(pq[i].ff>y)
+                        {
+                                ans+=pq[i].ff-y;
+                                y=max(y,pq[i].ss);
+                        }
+                        else
+                                y=max(y,pq[i].ss);
+             }
+             
+             ans+=(f-y);
+            if(ans>=k)
+            cout<<"YES"<<endl;
+            else
+            cout<<"NO"<<endl;
+                                                
+                            
+       }                             
+         
+}
